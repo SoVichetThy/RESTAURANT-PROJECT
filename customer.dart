536d0deb6abb1684e8e5df59081b7ef0a3698fc3
KZE-THONG <155614637+KZE-THONG@users.noreply.github.com>
2024-10-23 23:03:12 +0700

    testing1

--- customer.dart
+++ customer.dart
@@ -1,17 +1,17 @@
 class Customer {
   final int customerId;
   final String name;
-  final String phoneNumber;
+  final String phoneNumber1;
   final String? email;
   Customer(
       {required this.customerId,
       required this.name,
-      required this.phoneNumber,
+      required this.phoneNumber1,
       this.email});
   int? age;
   int? agee;
   @override
   String toString() {
-    return 'Customer ID: $customerId, Name: $name, Phone Number: $phoneNumber, Email: $email';
+    return 'Customer ID: $customerId, Name: $name, Phone Number: $phoneNumber1, Email: $email';
   }
 }
